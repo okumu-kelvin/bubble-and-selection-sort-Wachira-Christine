03eebd5d561804137d335953e7fed2c69cd2b1ad
Wachira-Christine <mumbi.wachira@strathmore.edu>
2025-06-20 11:41:30 +0300

    implementation of sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,11 @@
-def bubble_sort(unsorted_list):
+def bubble_sort(un_list):
     # TODO: Implement bubble sort
-    pass
+    n = len(un_list)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            #Swap elements found greater than the next element
+            if un_list[j] > un_list[j + 1]:
+                un_list[j], un_list[j + 1] = un_list[j+1], un_list[j]
+
+    return un_list
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n = len(arr)
+
+    for i in range(n):
+        #finding the minimum element in the unsorted
+        min_idx = i
+        for j in range (i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        #Swaping element that is found to be less than the first unsorted element
+        arr[i], arr[min_idx]= arr[min_idx], arr[i]
+
+    return arr
